757e2be4d41270f9eedb5b28bce59d7e7406f60c
Frankllin151 <frankllinsilva300@gamil.com>
2023-02-28 10:30:41 -0300

    commit morming

--- assets/js/receita.js
+++ assets/js/receita.js
@@ -1,21 +1,16 @@
-var DrodownButton = function(){
-  
-  const massa = document.getElementById('massa');
-  const recheio = document.getElementById('recheio');
-  const preparo = document.getElementById('preparo');
-  const montagem = document.getElementById('montagem');
- massa.addEventListener('click', (e) =>{
-           console.log('clicked ms');
- });
- recheio.addEventListener('click', (e) =>{
-    console.log('clicked rc');
-});
-preparo.addEventListener('click', (e) =>{
-    console.log('clicked prp');
-});
-montagem.addEventListener('click', (e) =>{
-    console.log('clicked mtng');
-});
+576 
+819
 
+function LayoutReponsive(){
+    var  viewportLayout = window.innerWidth; 
+    if(viewportLayout < 960){
+       
+         document.querySelector('.align-items-start').classList.add('jus-donfig-caixa')
+    } else{
+      
+        document.querySelector('.align-items-start').classList.remove('jus-donfig-caixa');
+    }
 }
-DrodownButton();
+ window.onload = LayoutReponsive; 
+
+ window.addEventListener('resize' , LayoutReponsive);
